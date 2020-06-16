611c144b3cf4cb8bd815dd2951341c047d7b765a
Wesley Rou <wesleyrou@gmail.com>
2020-06-16 09:13:27 -0700

    added snapshot

--- src/App.test.js
+++ src/App.test.js
@@ -7,6 +7,8 @@ import ReactDOM from 'react-dom';
 // make the App component available
 import App from './App';
 
+import renderer from 'react-test-renderer';
+
 // this is the test case
 it('renders without crashing', () => {
   // first create a DOM element to render the component into
@@ -19,8 +21,10 @@ it('renders without crashing', () => {
   ReactDOM.unmountComponentAtNode(div);
 });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+it('renders the UI as expected', () => {
+  const tree = renderer
+    .create(<App />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();  
+  });
+
